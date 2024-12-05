669aabab31ca1d4883c5fe3be3324805cdba1002
Cat5671 <boomer_dog@mail.ru>
2024-12-05 13:26:12 +0300

    feat: add main window

--- mainWindow.py
+++ mainWindow.py
@@ -1,13 +1,14 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit
+from layoutWordProccessor1 import Ui_wordProccessor
+
+class WordProccessor(QMainWindow, Ui_wordProccessor):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = QtWidgets.QWidget()
-window.show()
+app = QApplication(sys.argv)
+ex = WordProccessor()
+ex.show()
 sys.exit(app.exec_())
-
-
-
-
-
