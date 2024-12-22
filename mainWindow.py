dfe41a3d4c7e7c3e21c989c5cf9bd131f901df5f
Cat5671 <boomer_dog@mail.ru>
2024-12-22 16:10:37 +0300

    feat: add automatically adding a sheet

--- mainWindow.py
+++ mainWindow.py
@@ -1,9 +1,45 @@
 import sys
 import docx
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit, QFileDialog
-from layoutWordProccessor1 import Ui_wordProccessor
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLineEdit, QFileDialog, QTextEdit
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from layoutWordProccessor import Ui_wordProccessor
 from docx import Document
 import os
+class Sheet():
+    __instance = None
+    def __init__(self, sheetsScrollArea, verticalLayout):
+        self.sheet = QTextEdit(sheetsScrollArea)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sheet.sizePolicy().hasHeightForWidth())
+        self.sheet.setSizePolicy(sizePolicy)
+        self.sheet.setMinimumSize(QtCore.QSize(990, 1400))
+        self.sheet.setMaximumSize(QtCore.QSize(990, 1400))
+        self.sheet.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.sheet.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+                                 "QMenuBar::item:selected {\n"
+                                 "     background-color: #AAAAAA;\n"
+                                 " }")
+        verticalLayout.addWidget(self.sheet)
+        self.sheet.setStyleSheet("""
+                    QTextEdit {
+                        background-color: #FFFFFF;
+                        font-family: 'Calibri', sans-serif;  /* Шрифт, как в Word */
+                        font-size: 11pt;  /* Размер шрифта */
+                        padding-left: 144px;     /* Отступ слева (1.27 см) */
+                        padding-right: 72px;    /* Отступ справа (1.27 см) */
+                        padding-top: 96px;      /* Отступ сверху (1.27 см) */
+                        padding-bottom: 96px;   /* Отступ снизу (1.27 см) */
+                    }
+                """)
+        self.sheet.textChanged.connect(lambda: self.check_text_height(sheetsScrollArea, verticalLayout))
+
+    def check_text_height(self, sheetsScrollArea, verticalLayout):
+        if self.sheet.cursorRect(self.sheet.textCursor()).y() + self.sheet.cursorRect().height() > 1208 and self.__instance == None:
+            self.__instance = Sheet(sheetsScrollArea, verticalLayout)
+
 
 class WordProccessor(QMainWindow, Ui_wordProccessor):
     def __init__(self):
@@ -12,25 +48,18 @@ class WordProccessor(QMainWindow, Ui_wordProccessor):
         self.resize(800, 600)
         self.current_file_path = None
 
+
+        self.sheet1 = Sheet(self.sheetsScrollArea, self.verticalLayout_3)
+
+
         self.saveDocument.triggered.connect(self.save_fast)
         self.saveDocumentWhere.triggered.connect(self.save_as_docx)
         self.openDocument.triggered.connect(self.open_document)
         self.addDocument.triggered.connect(self.new_document)
-        self.document.setStyleSheet("""
-            QTextEdit {
-                background-color: #FFFFFF;
-                font-family: 'Calibri', sans-serif;  /* Шрифт, как в Word */
-                font-size: 11pt;  /* Размер шрифта */
-                padding-left: 144px;     /* Отступ слева (1.27 см) */
-                padding-right: 72px;    /* Отступ справа (1.27 см) */
-                padding-top: 96px;      /* Отступ сверху (1.27 см) */
-                padding-bottom: 96px;   /* Отступ снизу (1.27 см) */
-            }
-        """)
 
     def new_document(self):
         self.current_file_path = None
-        self.document.setPlainText("")
+        self.sheet.setPlainText("")
 
     def save_fast(self):
         if self.current_file_path:
@@ -47,7 +76,7 @@ class WordProccessor(QMainWindow, Ui_wordProccessor):
     def save_to_path(self, file_path):
         try:
             doc = Document()
-            text = self.document.toPlainText()
+            text = self.sheet.toPlainText()
 
             for line in text.splitlines():
                 doc.add_paragraph(line)
@@ -74,7 +103,7 @@ class WordProccessor(QMainWindow, Ui_wordProccessor):
                     text += paragraph.text + "\n"
 
 
-                self.document.setPlainText(text)
+                self.sheet.setPlainText(text)
                 self.current_file_path = file_path
                 self.statusBar().showMessage(f"Файл открыт: {file_path}", 5000)
 
@@ -85,7 +114,7 @@ class WordProccessor(QMainWindow, Ui_wordProccessor):
         # Устанавливаем отступы для QTextEdit, используя styleSheet
         left_margin, right_margin, top_margin, bottom_margin = margins_in_pixels
         print(margins_in_pixels)
-        self.document.setStyleSheet(f"""
+        self.sheet.setStyleSheet(f"""
             QTextEdit {{
                 background-color: #FFFFFF;
                 font-family: 'Calibri', sans-serif;
@@ -99,5 +128,6 @@ class WordProccessor(QMainWindow, Ui_wordProccessor):
 
 app = QApplication(sys.argv)
 ex = WordProccessor()
+
 ex.show()
 sys.exit(app.exec_())
